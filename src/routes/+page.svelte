0c416b81aa8df34fa8f41d1c241168df534e2960
Evan McClure <mcclure-evan@outlook.com>
2025-08-28 12:26:59 -0500

    copy update WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,10 +16,9 @@
     <img class="absolute top-0 left-0 w-full h-full object-cover z-0" alt="background" src="background.webp">
 
     <div class="mt-auto flex flex-col w-full md:w-2/3 z-10 gap-8 text-white md:absolute md:top-64 md:left-16">
-        <p class="text-5xl md:text-8xl font-fancy" style="filter: drop-shadow(0px 0px 10px #00000088);">Driving Growth Through <br> Tailored Solutions</p>
+        <p class="text-5xl md:text-8xl font-fancy" style="filter: drop-shadow(0px 0px 10px #00000088);">Driving Growth Through <br> Hands-On Activist Investment</p>
         <p class="text-xl md:text-2xl md:w-2/3">
-            With years of experience, we assist both large and small companies in reaching their targets through 
-            innovative approaches and measurable solutions
+            We partner with $1M-$10M EBITDA Founders to help scale their businesses 10x
         </p>
 
         <button on:click={() => document.getElementById("what-we-do")?.scrollIntoView({ behavior: "smooth" })} 
@@ -29,7 +28,7 @@
     <div class="hidden md:flex flex-col w-52 z-10 gap-4 text-white absolute bottom-16 right-16 p-4 bg-white bg-opacity-20 backdrop-blur-md">
         <button on:click={() => document.getElementById("testimonials")?.scrollIntoView({ behavior: "smooth" })} 
         class="w-full aspect-square bg-gray-400 text-white flex flex-col items-center justify-between p-5 transition-all hover:bg-gray-600 group">
-            <p class="font-bold text-xl">Testimonialz</p>
+            <p class="font-bold text-xl">Partners</p>
 
             <div class="w-16 aspect-square p-4 rounded-full bg-white bg-opacity-20 group-hover:bg-opacity-50 delay-100 transition-all">
                 <Arrow class="w-full h-full"/>
@@ -38,7 +37,7 @@
 
         <button on:click={() => document.getElementById("get-started")?.scrollIntoView({ behavior: "smooth" })}
         class="w-full aspect-square bg-blue-400 text-white flex flex-col items-center justify-between p-5 transition-all hover:bg-blue-500 group">
-            <p class="font-bold text-xl">Get Started? <br> Let's go</p>
+            <p class="font-bold text-xl">Want to scale? <br> Apply here</p>
 
             <div class="w-16 aspect-square p-4 rounded-full bg-white bg-opacity-20 group-hover:bg-opacity-50 delay-100 transition-all">
                 <Arrow class="w-full h-full rotate-90"/>
@@ -50,57 +49,55 @@
 <div id="what-we-do" class="w-full h-max md:h-[50rem] relative flex flex-col bg-gray-700 flex-shrink-0 items-center pt-16 p-8 md:p-16 md:justify-around gap-8">
     <div class="flex flex-col items-center w-full gap-4">
         <p class=" text-white">WHAT WE DO</p>
-        <p class="font-fancy text-white text-5xl md:text-7xl text-center md:text-left">We Offer Leading Solutions In</p>
+        <p class="font-fancy text-white text-5xl md:text-7xl text-center md:text-left">Revenue Growth + Cost Savings</p>
     </div>
     
     <div class="md:w-full justify-center flex flex-col md:flex-row md:h-80 gap-1">
         <div class="h-full w-72 bg-gray-500 flex flex-col p-6 gap-4 text-white">
             <Instagram class="w-16 h-16 bg-blue-400 p-4"/>
-            <p class="font-fancy text-4xl">Social Media <br> Management</p>
-            <p class="">Establish your brand's online presence with content tailored to your audience</p>
+            <p class="font-fancy text-4xl">Process Mapping <br> and Improvement</p>
+            <p class="">Work directly with on-site teams to document and make operations efficient</p>
         </div>
 
         <div class="h-full w-72 bg-gray-500 flex flex-col p-6 gap-4 text-white">
             <Globe class="w-16 h-16 bg-white text-gray-500 p-4"/>
-            <p class="font-fancy text-4xl">Internet <br> Presence</p>
-            <p class="">Grow your digital footprint with a professional website, effective SEO, and cohesive branding</p>
+            <p class="font-fancy text-4xl">Data <br> Warehousing</p>
+            <p class="">Consolidate data from disparate systems and create a single source of truth for reporting</p>
         </div>
 
         <div class="h-full w-72 bg-gray-500 flex flex-col p-6 gap-4 text-white">
             <Signal class="w-16 h-16 bg-blue-400 p-4"/>
-            <p class="font-fancy text-4xl">Ad <br> Campaigning</p>
-            <p class="">Drive targeted traffic and boost engagement with data-driven ad campaigns</p>
+            <p class="font-fancy text-4xl">Drive <br> Sales</p>
+            <p class="">Bring in our in-house sales team to scale revenue - including branding and social media</p>
         </div>
     </div>
 </div>
 
 <div id="testimonials" class="w-full relative flex flex-col bg-blue-400 flex-shrink-0 items-center py-16 px-8 md:px-48 gap-16">
     <div class="flex flex-col w-full text-white gap-2 text-center md:text-left">
-        <p class="">DON'T JUST TAKE IT FROM US</p>
-        <p class="font-fancy text-5xl md:text-6xl">Here's what our clients have to say</p>
+        <p class="">OUR PARTNERS</p>
+        <p class="font-fancy text-5xl md:text-6xl">Some of our success storeis</p>
     </div>
 
     <div class="w-full h-full gap-4 grid grid-rows-2 grid-cols-2 text-white">
         <div class="flex flex-col row-span-1 col-span-2 items-start gap-4 bg-white text-gray-500 p-6 md:p-12">
             <Quote class="h-12 aspect-square text-blue-400"/>
             <p class="text-xl md:text-3xl">
-                Venture Engine built our social media from scratch and helped us target ads for the right local audience. 
-                We ask everyone in the chair where they found us from. It used to be all Google but now, more often 
-                than not, people are finding us through social media.
+                Over $1M in cost savings annually through shipping cost reduction
+                
             </p>
 
             <div class="flex flex-row items-center gap-4">
                 <img class="h-12 aspect-square rounded-full object-cover" alt="john" src="blank_profile.webp">
 
-                <p class="text-4xl text-blue-400 font-fancy">Christopher Lombardo</p>
+                <p class="text-4xl text-blue-400 font-fancy">Mercatalyst</p>
             </div>
         </div>
 
         <div class="flex flex-col row-span-1 col-span-2 items-start gap-4 bg-white text-gray-500 p-6 md:p-12">
             <Quote class="h-12 aspect-square text-blue-400"/>
             <p class="text-xl md:text-3xl">
-                We cut our vacancy rate in half when Venture Engine redid our website and social media. Customers have 
-                started coming to us instead of the other way around.
+                Telos took the info from all our different systems and put them in one place. We stopped having to chase down why one number doesn't match another.
             </p>
 
             <div class="flex flex-row items-center gap-4">
